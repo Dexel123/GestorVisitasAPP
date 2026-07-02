0a89da8f2c0c23f087679e6121f3c6649dad6ccd
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-07-01 22:08:19 -0600

    feat(javafx):Se actualizo la mustra de datos de las entidades desde JavaFX

--- GestorVisitasAPP/src/org/gestorvisitasapp/fxml/MenuPrincipalFXController.java
+++ GestorVisitasAPP/src/org/gestorvisitasapp/fxml/MenuPrincipalFXController.java
@@ -25,20 +25,20 @@ import org.gestorvisitasapp.model.Vehiculos;
 import org.gestorvisitasapp.model.Visitas;
 
 public class MenuPrincipalFXController implements Initializable {
-    
-    @FXML private Label lblTitulo; 
-    @FXML private TableView tablaView; 
+
+    @FXML private Label lblTitulo;
+    @FXML private TableView tablaView;
     @FXML private Button btnVisitantes;
     @FXML private Button btnSecretarios;
     @FXML private Button btnAreas;
     @FXML private Button btnGafetes;
     @FXML private Button btnVehiculos;
     @FXML private Button btnVisitas;
-     
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }    
-    
+    }
+
     @FXML
     private void cargarVisitantes() {
         tablaView.getColumns().clear();
@@ -50,14 +50,12 @@ public class MenuPrincipalFXController implements Initializable {
         colApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
         TableColumn<Visitantes, String> colTipo = new TableColumn<>("Tipo");
         colTipo.setCellValueFactory(new PropertyValueFactory<>("tipoVisitante"));
-        TableColumn<Visitantes, String> colTelefono = new TableColumn<>("Telefono");
-        colTelefono.setCellValueFactory(new PropertyValueFactory<>("telefonoContacto"));
-        tablaView.getColumns().addAll(colId, colNombres, colApellidos, colTipo, colTelefono);
+        tablaView.getColumns().addAll(colId, colNombres, colApellidos, colTipo);
         ObservableList<Visitantes> lista = FXCollections.observableArrayList(new VisitantesDAOImpl().listar());
         tablaView.setItems(lista);
         lblTitulo.setText("Modulo de Visitantes");
     }
-    
+
     @FXML
     private void cargarSecretarios() {
         tablaView.getColumns().clear();
@@ -69,85 +67,73 @@ public class MenuPrincipalFXController implements Initializable {
         colApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
         TableColumn<Secretarios, String> colCargo = new TableColumn<>("Cargo");
         colCargo.setCellValueFactory(new PropertyValueFactory<>("cargoPuesto"));
-        TableColumn<Secretarios, String> colJornada = new TableColumn<>("Jornada");
-        colJornada.setCellValueFactory(new PropertyValueFactory<>("jornadaLaboral"));
-        tablaView.getColumns().addAll(colId, colNombres, colApellidos, colCargo, colJornada);
+        tablaView.getColumns().addAll(colId, colNombres, colApellidos, colCargo);
         ObservableList<Secretarios> lista = FXCollections.observableArrayList(new SecretariosDAOImpl().listar());
         tablaView.setItems(lista);
         lblTitulo.setText("Modulo de Secretarios");
     }
-    
+
     @FXML
     private void cargarAreas() {
         tablaView.getColumns().clear();
         TableColumn<Areas, Integer> colId = new TableColumn<>("ID");
         colId.setCellValueFactory(new PropertyValueFactory<>("idArea"));
-        TableColumn<Areas, String> colNombre = new TableColumn<>("Nombre Area");
+        TableColumn<Areas, String> colNombre = new TableColumn<>("Area");
         colNombre.setCellValueFactory(new PropertyValueFactory<>("nombreArea"));
-        TableColumn<Areas, String> colSalon = new TableColumn<>("Salon");
-        colSalon.setCellValueFactory(new PropertyValueFactory<>("numeroSalonTaller"));
-        TableColumn<Areas, String> colSector = new TableColumn<>("Sector");
-        colSector.setCellValueFactory(new PropertyValueFactory<>("sectorEdificio"));
         TableColumn<Areas, String> colCarrera = new TableColumn<>("Carrera");
         colCarrera.setCellValueFactory(new PropertyValueFactory<>("carreraTecnicaAsignada"));
-        tablaView.getColumns().addAll(colId, colNombre, colSalon, colSector, colCarrera);
+        tablaView.getColumns().addAll(colId, colNombre, colCarrera);
         ObservableList<Areas> lista = FXCollections.observableArrayList(new AreasDAOImpl().listar());
         tablaView.setItems(lista);
         lblTitulo.setText("Modulo de Areas");
     }
-    
+
     @FXML
     private void cargarGafetes() {
-    tablaView.getColumns().clear();
-    TableColumn<Gafetes, Integer> colId = new TableColumn<>("ID");
-    colId.setCellValueFactory(new PropertyValueFactory<>("idGafete"));
-    TableColumn<Gafetes, String> colTipo = new TableColumn<>("Tipo");
-    colTipo.setCellValueFactory(new PropertyValueFactory<>("tipoGafete"));
-    TableColumn<Gafetes, String> colEstado = new TableColumn<>("Estado");
-    colEstado.setCellValueFactory(new PropertyValueFactory<>("estadoGafete"));
-    TableColumn<Gafetes, String> colDireccion = new TableColumn<>("Direccion");
-    colDireccion.setCellValueFactory(new PropertyValueFactory<>("direccionVisita"));
-    TableColumn<Gafetes, String> colVuelta = new TableColumn<>("Estado Vuelta");
-    colVuelta.setCellValueFactory(new PropertyValueFactory<>("estadoVuelta"));
-    tablaView.getColumns().addAll(colId, colTipo, colEstado, colDireccion, colVuelta);
-    ObservableList<Gafetes> lista = FXCollections.observableArrayList(new GafetesDAOImpl().listar());
-    tablaView.setItems(lista);
-    lblTitulo.setText("Modulo de Gafetes");
+        tablaView.getColumns().clear();
+        TableColumn<Gafetes, Integer> colId = new TableColumn<>("ID");
+        colId.setCellValueFactory(new PropertyValueFactory<>("idGafete"));
+        TableColumn<Gafetes, String> colTipo = new TableColumn<>("Tipo");
+        colTipo.setCellValueFactory(new PropertyValueFactory<>("tipoGafete"));
+        TableColumn<Gafetes, String> colEstado = new TableColumn<>("Estado");
+        colEstado.setCellValueFactory(new PropertyValueFactory<>("estadoGafete"));
+        tablaView.getColumns().addAll(colId, colTipo, colEstado);
+        ObservableList<Gafetes> lista = FXCollections.observableArrayList(new GafetesDAOImpl().listar());
+        tablaView.setItems(lista);
+        lblTitulo.setText("Modulo de Gafetes");
     }
-    
+
     @FXML
     private void cargarVehiculos() {
-    tablaView.getColumns().clear();
-    TableColumn<Vehiculos, String> colPlaca = new TableColumn<>("Placa");
-    colPlaca.setCellValueFactory(new PropertyValueFactory<>("placa"));
-    TableColumn<Vehiculos, String> colTipo = new TableColumn<>("Tipo");
-    colTipo.setCellValueFactory(new PropertyValueFactory<>("tipo_vehiculo"));
-    TableColumn<Vehiculos, String> colMarca = new TableColumn<>("Marca");
-    colMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
-    TableColumn<Vehiculos, String> colColor = new TableColumn<>("Color");
-    colColor.setCellValueFactory(new PropertyValueFactory<>("color"));
-    tablaView.getColumns().addAll(colPlaca, colTipo, colMarca, colColor);
-    ObservableList<Vehiculos> lista = FXCollections.observableArrayList(new VehiculosDAOImpl().listar());
-    tablaView.setItems(lista);
-    lblTitulo.setText("Modulo de Vehiculos");
+        tablaView.getColumns().clear();
+        TableColumn<Vehiculos, String> colPlaca = new TableColumn<>("Placa");
+        colPlaca.setCellValueFactory(new PropertyValueFactory<>("placa"));
+        TableColumn<Vehiculos, String> colTipo = new TableColumn<>("Tipo");
+        colTipo.setCellValueFactory(new PropertyValueFactory<>("tipoVehiculo"));
+        TableColumn<Vehiculos, String> colColor = new TableColumn<>("Color");
+        colColor.setCellValueFactory(new PropertyValueFactory<>("colorVehiculo"));
+        tablaView.getColumns().addAll(colPlaca, colTipo, colColor);
+        ObservableList<Vehiculos> lista = FXCollections.observableArrayList(new VehiculosDAOImpl().listar());
+        tablaView.setItems(lista);
+        lblTitulo.setText("Modulo de Vehiculos");
     }
-    
+
     @FXML
     private void cargarVisitas() {
-    tablaView.getColumns().clear();
-    TableColumn<Visitas, Integer> colId = new TableColumn<>("ID");
-    colId.setCellValueFactory(new PropertyValueFactory<>("id_visita"));
-    TableColumn<Visitas, String> colNombres = new TableColumn<>("Nombres");
-    colNombres.setCellValueFactory(new PropertyValueFactory<>("nombres"));
-    TableColumn<Visitas, String> colMotivo = new TableColumn<>("Motivo");
-    colMotivo.setCellValueFactory(new PropertyValueFactory<>("motivo_visita"));
-    TableColumn<Visitas, String> colArea = new TableColumn<>("Area");
-    colArea.setCellValueFactory(new PropertyValueFactory<>("nombre_area"));
-    TableColumn<Visitas, String> colFecha = new TableColumn<>("Entrada");
-    colFecha.setCellValueFactory(new PropertyValueFactory<>("fecha_hora_entrada"));
-    tablaView.getColumns().addAll(colId, colNombres, colMotivo, colArea, colFecha);
-    ObservableList<Visitas> lista = FXCollections.observableArrayList(new VisitasDAOImpl().listar());
-    tablaView.setItems(lista);
-    lblTitulo.setText("Modulo de Visitas");
-    } 
+        tablaView.getColumns().clear();
+        TableColumn<Visitas, Integer> colId = new TableColumn<>("ID");
+        colId.setCellValueFactory(new PropertyValueFactory<>("id_visita"));
+        TableColumn<Visitas, String> colNombres = new TableColumn<>("Nombres");
+        colNombres.setCellValueFactory(new PropertyValueFactory<>("nombres"));
+        TableColumn<Visitas, String> colMotivo = new TableColumn<>("Motivo");
+        colMotivo.setCellValueFactory(new PropertyValueFactory<>("motivo_visita"));
+        TableColumn<Visitas, String> colArea = new TableColumn<>("Area");
+        colArea.setCellValueFactory(new PropertyValueFactory<>("nombre_area"));
+        TableColumn<Visitas, String> colFecha = new TableColumn<>("Entrada");
+        colFecha.setCellValueFactory(new PropertyValueFactory<>("fecha_hora_entrada"));
+        tablaView.getColumns().addAll(colId, colNombres, colMotivo, colArea, colFecha);
+        ObservableList<Visitas> lista = FXCollections.observableArrayList(new VisitasDAOImpl().listar());
+        tablaView.setItems(lista);
+        lblTitulo.setText("Modulo de Visitas");
+    }
 }
